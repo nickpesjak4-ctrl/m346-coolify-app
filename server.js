33c817257c4c93b2d943e806eedbb6ab64aa6ea9
Nick Pesjak <nick.pesjak@stud.bfstg.ch>
2026-05-31 10:41:12 +0200

    fix coolify app

--- server.js
+++ server.js
@@ -2,24 +2,25 @@ const express = require('express');
 const { Client } = require('pg');
 
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
 });
 
 client.connect()
-  .then(() => console.log('Database connected'))
-  .catch(err => console.log(err));
+    .then(() => console.log('Database connected'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Coolify App läuft!');
+    res.send('Coolify App läuft!');
 });
 
 app.get('/health', (req, res) => {
-  res.status(200).send('OK');
+    res.status(200).send('OK');
 });
 
-app.listen(port, () => {
-  console.log(`Server läuft auf Port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server läuft auf Port ${port}`);
 });
