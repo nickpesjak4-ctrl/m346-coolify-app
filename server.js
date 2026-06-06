04a23463173559bf67db32191a84e60d1a28a28b
Nick Pesjak <nick.pesjak@stud.bfstg.ch>
2026-06-06 16:36:02 +0200

    Website wieder funktionsfähig

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const { Client } = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
 });
@@ -14,42 +12,8 @@ client.connect()
     .then(() => console.log('Database connected'))
     .catch(err => console.log(err));
 
-app.get('/', async (req, res) => {
-    try {
-        const result = await client.query(
-            'SELECT * FROM fahrzeuge ORDER BY marke'
-        );
-
-        let html = `
-        <html>
-        <head>
-            <title>Fahrzeugverwaltung</title>
-        </head>
-        <body>
-            <h1>🚗 Fahrzeugverwaltung</h1>
-            <h3>Meine Fahrzeuge</h3>
-        `;
-
-        result.rows.forEach(fahrzeug => {
-            html += `
-                <div style="border:1px solid black;padding:10px;margin:10px;">
-                    <h2>${fahrzeug.marke} ${fahrzeug.modell}</h2>
-                    <p>Baujahr: ${fahrzeug.baujahr}</p>
-                    <p>Kilometer: ${fahrzeug.kilometer}</p>
-                </div>
-            `;
-        });
-
-        html += `
-        </body>
-        </html>
-        `;
-
-        res.send(html);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Fehler beim Laden der Fahrzeuge');
-    }
+app.get('/', (req, res) => {
+    res.send('Coolify App läuft!');
 });
 
 app.get('/health', (req, res) => {
